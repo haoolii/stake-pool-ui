3b858624644d84d754efa70f9dbc86dcfe6a0f10
Hao <unnhao@gmail.com>
2022-03-13 16:11:44 +0800

    (chore) add gitbook link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,33 +16,35 @@ export default function Header() {
     (state) => state.account
   );
 
-  const accuntDisplay = account ? `${account.slice(0, 6)} ... ${account.slice(account.length - 4, account.length)}` : null;
+  const accuntDisplay = account
+    ? `${account.slice(0, 6)} ... ${account.slice(
+        account.length - 4,
+        account.length
+      )}`
+    : null;
 
   return (
     <div className="fixed top-0 container mx-auto">
       <div className="w-full flex justify-between">
         <div className="flex space-x-4 py-4">
-          <div>
-            <CustomLink to="/">Logo</CustomLink>
-          </div>
-          <div>
-            <CustomLink to="/stake">Stake</CustomLink>
-          </div>
-          <div>
-            <button className="leading-5 text-sm rounded-lg text-slate-600 font-bold hover:bg-slate-200 py-2 px-4">
-              Docs
-            </button>
-          </div>
+          <CustomLink to="/">Logo</CustomLink>
+          <CustomLink to="/stake">Stake</CustomLink>
+          <a
+            href="https://unnhao.gitbook.io/hao-stake-pool/"
+            target="_blank"
+            className="leading-5 text-sm rounded-lg text-slate-600 font-bold hover:bg-slate-200 py-2 px-4"
+          >
+            Docs
+          </a>
         </div>
         <div className="flex py-4">
-          
           {accuntDisplay ? (
             <button className="w-36 text-ellipsis overflow-hidden leading-5 text-sm rounded-lg text-slate-600 font-bold hover:bg-slate-200 py-2 px-4">
               {accuntDisplay}
             </button>
           ) : (
             <button className="w-36 leading-5 text-sm rounded-lg text-slate-600 font-bold hover:bg-slate-200 py-2 px-4">
-              {loading ? "Loading" : error ? error : "Connect Wallet"}
+              {loading ? 'Loading' : error ? error : 'Connect Wallet'}
             </button>
           )}
         </div>
@@ -58,7 +60,9 @@ function CustomLink({ children, to, ...props }: LinkProps) {
   return (
     <div className="flex items-center">
       <Link
-        className={`leading-5 text-sm rounded-lg text-slate-600 font-bold hover:bg-slate-200 py-2 px-4 ${match ? 'bg-slate-200' : ''}`}        
+        className={`leading-5 text-sm rounded-lg text-slate-600 font-bold hover:bg-slate-200 py-2 px-4 ${
+          match ? 'bg-slate-200' : ''
+        }`}
         to={to}
         {...props}
       >
